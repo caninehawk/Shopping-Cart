add0798de6a3a4649750bb5f521ccaff3c4f7f2a
Sandeep <k.sandeep.shandilya39@gmail.com>
2024-07-03 13:16:20 -0700

    Added Loading bar

--- ShoppingCart/src/Cards.jsx
+++ ShoppingCart/src/Cards.jsx
@@ -1,28 +1,38 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import './Cards.css';
+import Loading from './Loading.jsx';
 
-import './Cards.css'
-
-const Cards = (props) => {
+const Cards = ({ productURL }) => {
     const [cards, updateCards] = useState([]);
     const location = useLocation();
     const [kart, updateKart] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         const fetchData = async () => {
+            setLoading(true);  // Ensure loading is true at the start of the fetch
             try {
-                const response = await fetch(props.productURL);
+                const response = await fetch(productURL);
                 const data = await response.json();
                 updateCards(data);
             } catch (error) {
                 console.error("Error Fetching Information", error);
+            } finally {
+                setLoading(false);  // Set loading to false after fetch is complete
             }
         };
         fetchData();
-    }, [location, props.productURL]);
-    useEffect(() => {
-        
-    })
-    
+    }, [location, productURL]);
+
+    const handleAddToKart = (product) => {
+        updateKart((prevKart) => [...prevKart, product]);
+    };
+
+    if (loading) {
+        return <Loading />;
+    }
+
     return (
         <div className="card-container">
             <div className="card-grid">
@@ -31,7 +41,7 @@ const Cards = (props) => {
                         <h4>{product.title}</h4>
                         <img src={product.image} alt={product.title} className="card-img" />
                         <h4>Price: ${product.price}</h4>
-                        <button onClick={() => {updateKart(...kart, cards[index])}}>Add to Kart</button>
+                        <button onClick={() => handleAddToKart(cards[index])}>Add to Kart</button>
                     </div>
                 ))}
             </div>
---
+++ ShoppingCart/src/Loading.jsx
@@ -0,0 +1,10 @@
+import loading from './assets/loading.svg'
+import './Loading.css'
+const Loading = () => {
+    return (
+        <>
+            <img src={loading} alt="Loading ..." className='loading'/>
+        </>
+    )
+}
+export default Loading;
