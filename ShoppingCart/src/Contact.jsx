d9cde4159a240de3d3b2756e7e3e94f48e55ddd2
Sandeep <k.sandeep.shandilya39@gmail.com>
2024-07-03 15:53:47 -0700

    Finished

--- ShoppingCart/src/Contact.jsx
+++ ShoppingCart/src/Contact.jsx
@@ -1,6 +1,10 @@
 const Contact = () =>{
+    const imgurl = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzc0NnV6NWM5cTd4aTVibXlxeDUwczZkOXQ2Mm15YXlhMHAzM2JlNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/XVbQsIjdXDNyswwxOO/giphy.gif";
     return (
-        <h2>Contact</h2>
+        <div className="contact">
+            <img src={imgurl} alt="Evil Smirk.." className="evil"/>
+            <h2> you need to order before you can contact me...</h2>
+        </div>
     )
 }
 
